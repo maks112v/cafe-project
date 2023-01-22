10b059fba737b1415449ce3921dae25e544fbd24
maks <11082252+maks112v@users.noreply.github.com>
2023-01-21 20:50:12 -0500

    added auth and tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: './src/components',
+			$lib: './src/lib',
+			$sections: './src/sections',
+			$utils: './src/utils'
+		}
 	}
 };
 
